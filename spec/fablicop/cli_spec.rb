663d7c633c4d8189663c1e6d036dee72e772c65f
Yutaka Kamei <kamei@yykamei.me>
2021-08-10 16:51:10 +0900

    fixup! Add tests for Fablicop::CLI

--- spec/fablicop/cli_spec.rb
+++ spec/fablicop/cli_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe Fablicop::CLI do
     end
 
     context 'when init is passed' do
+      let(:content) { <<~YAML }
+        inherit_gem:
+          fablicop:
+            - "config/.base_rubocop.yml"
+      YAML
       around do |example|
         Dir.mktmpdir do |dir|
           Dir.chdir(dir) do
@@ -21,10 +26,18 @@ RSpec.describe Fablicop::CLI do
         end
       end
 
-      it 'generates .rubocop.yml' do
+      it 'creates .rubocop.yml' do
         expect { described_class.start(%w[init]) }
           .to output(<<~OUTPUT).to_stdout.and change { Pathname('.rubocop.yml').file? }.from(false).to(true).and raise_error(SystemExit)
           create .rubocop.yml
+        OUTPUT
+      end
+
+      it 'overwrites .rubocop.yml' do
+        Pathname('.rubocop.yml').write('---')
+        expect { described_class.start(%w[init]) }
+          .to output(<<~OUTPUT).to_stdout.and change { Pathname('.rubocop.yml').read }.from('---').to(content).and raise_error(SystemExit)
+          overwrite .rubocop.yml
         OUTPUT
       end
     end
